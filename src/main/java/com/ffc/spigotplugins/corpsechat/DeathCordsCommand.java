1450eacf6626d293bda3159d7bf0902ee9fbf8b6
MCHSoda <MCHGECKO@GMAIL.COM>
2021-03-15 16:32:27 -0400

    "else" error fixed

---
+++ src/main/java/com/ffc/spigotplugins/corpsechat/DeathCordsCommand.java
@@ -0,0 +1,22 @@
+package com.ffc.spigotplugins.corpsechat;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DeathCordsCommand implements CommandExecutor {
+    private final Main main;
+    public DeathCordsCommand(Main main){
+        this.main = main;
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            main.getLogger().info("You have to be a player to get cords");
+        }
+        return false;
+    }
+}
